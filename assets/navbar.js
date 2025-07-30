5917f2c87aacd476a4aee68773581bbaf981b5dc
arthurfurtunato <arthurfurtunato@gmail.com>
2025-07-30 11:57:58 -0300

    Ajuste do aparelho celular e mais alguns ajustes na tela e mudanças de imagens

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,5 +1,32 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('open');
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+menuToggle.addEventListener("click", (e) => {
+  e.stopPropagation(); // Evita que o clique no botão feche o menu imediatamente
+  navLinks.classList.toggle("open");
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    navLinks.classList.contains("open") &&
+    !navLinks.contains(e.target) &&
+    e.target !== menuToggle
+  ) {
+    navLinks.classList.remove("open");
+  }
+});
+
+document.getElementById("link-inicio").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+document.getElementById("icone-inicio").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
